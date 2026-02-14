f5ea287fd354c5c35bdfd9f3c5dd9104b3825883
CloudSecManagement <cloud.management@harrywellsagency.com>
2026-02-14 18:53:44 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: '', // us-east-1_2OavjoRGU
-        userPoolClientId: '', // 2mvbvhjhgps22ha4h31grm97l9
-        region: '' // us-east-1
+        userPoolId: '', us-east-1_L7fvhJz4F
+        userPoolClientId: '', 6bqpfl0iaah3clns4m1enr7ih0
+        region: '' us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
